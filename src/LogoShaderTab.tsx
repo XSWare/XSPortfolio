4c5d87c69940984aafbead33d4083c0f22982320
CaptainOachkatzl <dave.hofer@hotmail.com>
2025-08-12 13:40:17 +0200

    added descriptions

--- src/LogoShaderTab.tsx
+++ src/LogoShaderTab.tsx
@@ -7,8 +7,10 @@ function LogoShaderTab() {
     return (
         <>
             <NavBar />
-            <div style={{ minHeight: "100vh", justifyContent: "center", alignItems: "center", display: "flex" }}>
+            <div style={{ minHeight: "100vh", justifyContent: "center", alignItems: "center", display: "flex", flexDirection: "column" }}>
+                <h3>Hover the image with your mouse pointer to apply the effect. Click the image to change the effect.</h3>
                 <LogoShader />
+                <text>This preview uses <a href='https://gpuweb.github.io/gpuweb/'>WebGPU</a> to apply shader effects to a static image.</text>
             </div>
         </>
     )
--- src/SlicingGameTab.tsx
+++ src/SlicingGameTab.tsx
@@ -7,8 +7,10 @@ function SlicingGameTab() {
     return (
         <>
             <NavBar />
-            <div style={{ minHeight: "100vh", justifyContent: "center", alignItems: "center", display: "flex" }}>
+            <div style={{ minHeight: "100vh", justifyContent: "center", alignItems: "center", display: "flex", flexDirection: "column" }}>
+                <h3>Click the red block to slice it up as often as you can!</h3>
                 <ShatterGame />
+                <text>This <a href='http://bevy.org'>Bevy</a> app uses dynamic vertex slicing to generate new meshes on the fly.</text>
             </div>
         </>
     )
