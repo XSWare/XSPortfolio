08d405d0a602b65ff791e551d201b697484794e5
CaptainOachkatzl <dave.hofer@hotmail.com>
2025-08-22 11:34:37 +0200

    disclaimer

--- src/Home.tsx
+++ src/Home.tsx
@@ -5,8 +5,9 @@ function Home() {
     return (
         <>
             <NavBar isSPA={true} />
-            <div style={{ minHeight: "100vh", justifyContent: "center", alignItems: "center", display: "flex" }}>
+            <div style={{ minHeight: "100vh", justifyContent: "center", alignItems: "center", display: "flex", flexDirection: "column" }}>
                 <h1>Welcome to the xsware homepage!</h1>
+                <pre>This website is currently being built, so some things may be missing or not work, but feel free to check out what is already there!</pre>
             </div>
         </>
     )
