868ae4d6b2df2b13d97ba7e660e75cceb972293f
CaptainOachkatzl <dave.hofer@hotmail.com>
2025-08-23 11:34:18 +0200

    moved styles to CSS file

--- src/OrderBook.tsx
+++ src/OrderBook.tsx
@@ -3,23 +3,23 @@ import NavBar from './components/NavBar';
 import OrderForm from './components/OrderForm';
 import PriceHistory from './components/PriceHistory';
 import PriceLevels from './components/PriceLevels';
-import "./Orderbook.css"
+import "./OrderBook.css"
 
 function OrderBook() {
     const [status, setStatus] = useState('');
     return (
         <>
             <NavBar isSPA={true} />
-            <div style={{ minHeight: "450px", minWidth: "900px" }}>
-                <div style={{ display: 'flex', flexDirection: 'row', height: "80vh", margin: "20px 0 0 0" }} >
-                    <div style={{ width: "70%", height: "auto", maxHeight: "800px" }}>
+            <div className="order-book-container">
+                <div className="order-book-main">
+                    <div className="price-history-container">
                         <PriceHistory />
                     </div>
-                    <div>
-                        <div style={{ height: "70%", margin: "0 0 10px 0" }}>
+                    <div className="sidebar-container">
+                        <div className="price-levels-container">
                             <PriceLevels />
                         </div>
-                        <div style={{ width: "80%", alignItems: "center", display: "flex", justifyContent: "center", flexDirection: "column", margin: "0 auto" }}>
+                        <div className="order-form-container">
                             <OrderForm setStatus={setStatus} />
                         </div>
                     </div>
